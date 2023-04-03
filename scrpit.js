18a07d67e4d72ad7b3db889810921a68689f75da
Roberttamyo56 <reytamyo1976@gmail.com>
2023-04-03 13:10:17 -0500

    subida trabajo calculadora

---
+++ scrpit.js
@@ -0,0 +1,62 @@
+window.addEventListener('load',() => {
+    const keyButtons = document.getElementsByClassName('keypard-button');
+    const display = document.querySelector('.calculator-display');
+
+    const keyButtonsArray = Array.from(keyButtons);
+
+    keyButtonsArray.forEach( (button)=> {
+        button.addEventListener('click', () => {
+            console.log(button)
+            calculadora(button,display)
+        });
+        
+    });
+});
+
+function calculadora (button,display){
+    switch (button.innerHTML){
+        case 'C':
+            borrar(display);
+        break;
+        case '=':
+            resultado(display);
+            break;
+        case '←':
+            eliminar (display)
+            break
+            default:
+                actualizar(display,button);
+                break;
+                
+    
+    }
+
+}
+function resultado(display) {
+   display.innerHTML= eval (display.innerHTML)
+    
+}
+function actualizar(display,button) {
+    if (display.innerHTML==0){
+        display.innerHTML = '';
+    }
+    display.innerHTML+= button.innerHTML
+}
+function borrar(display) {
+    if (display.innerHTML !=0){
+        display.innerHTML= 0
+
+    }
+    
+}
+function eliminar(display){
+    if (display.innerHTML !=0){
+        display.innerHTML= display.innerHTML.slice (0,display.innerHTML.length -1)
+    }
+        
+    
+}
+
+ 
+
+    
